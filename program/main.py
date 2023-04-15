baceea5c49807028054c17b7957e4555818dc2ca
SSBO <anderssjohansson@gmail.com>
2023-04-15 15:44:46 +0200

    Added more functionality

--- program/main.py
+++ program/main.py
@@ -1,4 +1,22 @@
-
+from func_connections import connect_dydx
+from constants import ABORT_ALL_POSITIONS
+from func_private import abort_all_positions
 
 if __name__ == "__main__":
-    print("Hello bot2g!")
+    
+    # Connect to client
+    try: 
+        print("Connection to client...")
+        client  = connect_dydx()
+    except Exception as e:
+        print("Error connecting to client: ", e)
+        exit(1)
+
+    # Abort all open positions
+    if ABORT_ALL_POSITIONS:
+        try:
+            print("Closing all positions...")
+            close_orders = abort_all_positions(client)
+        except Exception as e:
+            print("Error closing all positions: ", e)
+            exit(1)
